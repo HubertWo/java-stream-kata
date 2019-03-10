a15373b7d0fa03a7de2695e5c86a4df5ad59e92e
HubertWo <2518652+HubertWo@users.noreply.github.com>
2019-03-09 19:28:31 -0600

    Task: is palindrome candidate

---
+++ src/test/java/basics/PalindromeTest.java
@@ -0,0 +1,48 @@
+package basics;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class PalindromeTest {
+
+    /**
+     * Word is an palindrome candidate when each character of word occurs even number of
+     * times or only one character occurs odd number of times.
+     *
+     * @return true if {@param word} is an palindrome candidate
+     */
+    // <editor-fold defaultstate="collapsed" desc="Click here to see the answer">
+    /*
+        // Count occurrence of each character
+        final Map<Integer, Long> countedCharacters = word
+                .chars()
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        // Count number of characters which occurred odd number of times
+        final long oddCharactersCount = countedCharacters.values().stream()
+                .filter(count -> count % 2 != 0)
+                .count();
+
+        // Only one character with odd number of occurrences is allowed
+        return oddCharactersCount <= 1;
+    */
+    //</editor-fold>
+    private static boolean isPalindromeCandidate(final String word) {
+        // TODO: implement using {@link Stream}
+        throw new IllegalStateException("Not implemented yet");
+    }
+
+    @Test
+    @DisplayName("Is palindrome candidate")
+    void palindromeCandidate() {
+        assertThat(isPalindromeCandidate("a"), is(true));
+        assertThat(isPalindromeCandidate("ab"), is(false));
+        assertThat(isPalindromeCandidate("aab"), is(true));
+        assertThat(isPalindromeCandidate("aabb"), is(true));
+        assertThat(isPalindromeCandidate("aabbc"), is(true));
+    }
+}
