b1e9c7ac1b20a66424da7ef6bb3fc214848ef40f
HubertWo <2518652+HubertWo@users.noreply.github.com>
2020-08-08 11:58:04 +0200

    Task - String.lines

--- src/test/java/com/github/hubertwo/kata/stream/basics/BasicsJava11Test.java
+++ src/test/java/com/github/hubertwo/kata/stream/basics/BasicsJava11Test.java
@@ -4,23 +4,25 @@ import com.github.hubertwo.kata.stream.Fruit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-import java.util.function.Function;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
-import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toUnmodifiableList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.not;
 
 /**
  * By doing this tasks you will learn how to use:
  * <p>
  * - {@link Predicate#not}
+ * - {@link String#lines}
  * </p>
  *
  * @author https://github.com/HubertWo
@@ -28,6 +30,8 @@ import static org.assertj.core.api.Assertions.not;
 @SuppressWarnings("SimplifyStreamApiCallChains")
 @DisplayName("Stream basics - Java 11")
 class BasicsJava11Test {
+
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     private static final Fruit BANANA = new Fruit("Banana", 105);
     private static final Fruit PAPAYA = new Fruit("Papaya", 109);
@@ -47,7 +51,7 @@ class BasicsJava11Test {
     @Test
     @DisplayName("Task: Get all fruits except BANANA")
     void task1() {
-        List<Fruit> everythingExceptBanana = FRUITS.stream()
+        final List<Fruit> everythingExceptBanana = FRUITS.stream()
                 // TODO: put your answer here
                 .collect(toUnmodifiableList());
 
@@ -55,4 +59,28 @@ class BasicsJava11Test {
                 .doesNotContain(BANANA)
                 .containsExactlyInAnyOrder(PAPAYA, KIWI, MANGO, PEACH);
     }
+
+    // <editor-fold defaultstate="collapsed" desc="Click here to see the answer">
+    /*
+      givenFruitList
+                .lines()
+                .map(line -> line.split(","))
+                .map(splittedLine -> new Fruit(splittedLine[0], Integer.parseInt(splittedLine[1])))
+                .collect(toList());
+     */
+    //</editor-fold>
+    @Test
+    @DisplayName("Task: Load fruits list from file")
+    void task2() throws IOException, URISyntaxException {
+        // Loads the String from resources file
+        URI resource = ClassLoader.getSystemResource("java11/fruitList.txt").toURI();
+        final String givenFruitList = Files.readString(Paths.get(resource));
+
+        // TODO: convert givenFruitList to list using Streams
+        final List<Fruit> actualFruitList = null;
+
+        assertThat(actualFruitList)
+                .containsExactlyInAnyOrderElementsOf(FRUITS);
+    }
+
 }
