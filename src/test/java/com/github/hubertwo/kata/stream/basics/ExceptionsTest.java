570984d7bfa678e40ec9a987965da1e62e052dae
HubertWo <2518652+HubertWo@users.noreply.github.com>
2020-08-08 12:22:25 +0200

    Empty test file

---
+++ src/test/java/com/github/hubertwo/kata/stream/basics/ExceptionsTest.java
@@ -0,0 +1,18 @@
+package com.github.hubertwo.kata.stream.basics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * This tasks will show you have to mange with exception handling
+ * inside the streams.
+ */
+public class ExceptionsTest {
+
+    @Test
+    public void dummy() {
+        fail();
+    }
+
+}
